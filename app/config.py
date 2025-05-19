a10249c69d9961230d805a308f1ec92fd896e514
Shay-Hotoveli <shayhotav@gmail.com>
2025-05-19 22:38:37 +0300

    Passing jenkins image to compose file

--- app/config.py
+++ app/config.py
@@ -13,6 +13,5 @@ def get_db_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT"))
+        database=os.getenv("DB_NAME")
     )
