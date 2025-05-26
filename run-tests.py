cc22ca86c7ad9241985856ad995a986fdff3c1ae
Shay-Hotoveli <shayhotav@gmail.com>
2025-05-26 16:03:24 +0300

    Fixing environment typo

--- run-tests.py
+++ run-tests.py
@@ -11,10 +11,10 @@ def wait_for_mysql():
     for attempt in range(1, MAX_RETRIES + 1):
         try:
             connection = mysql.connector.connect(
-                host=os.getenv("DB_HOST", "db"),
-                user=os.getenv("DB_USER", "app_user"),
-                password=os.getenv("DB_PASSWORD", "123456"),
-                database=os.getenv("DB_NAME", "todo_app")
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME")
             )
             if connection.is_connected():
                 print("[PY] MySQL is ready.")
